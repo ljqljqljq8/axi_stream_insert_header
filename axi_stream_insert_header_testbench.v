e4784fa56b69a9b256f7db66d31d53aa2ca0651e
ljqljqljq8 <118333395+ljqljqljq8@users.noreply.github.com>
2024-01-29 14:48:57 +0800

    Update axi_stream_insert_header_testbench.v

--- axi_stream_insert_header_testbench.v
+++ axi_stream_insert_header_testbench.v
@@ -1,29 +1,33 @@
+`define DATA_WIDTH 64
+
 module skid_sim;
-  reg clk;
-  reg rst_n;
+  reg                                clk;
+  reg                                rst_n;
 
   // AXI Stream input original data
-  reg                    valid_in;
-  reg  [16-1:0]          data_in;
-  reg  [2-1:0]           keep_in;
-  reg                    last_in;
-  wire                   ready_in;
+  reg                                valid_in;
+  reg [`DATA_WIDTH-1:0]              data_in;
+  reg [(`DATA_WIDTH / 8)-1:0]        keep_in;
+  reg                                last_in;
+  wire                               ready_in;
 
   // AXI Stream output with header inserted
   wire                   valid_out;
-  wire  [16-1:0]         data_out;
-  wire  [2-1:0]          keep_out;
-  wire                   last_out;  
-  reg                    ready_out;
+  wire [`DATA_WIDTH-1:0]             data_out;
+  wire [(`DATA_WIDTH / 8)-1:0]       keep_out;
+  wire                               last_out;  
+  reg                                ready_out;
 
-  reg                    valid_insert;
-  reg  [16-1:0]          data_insert;
-  reg  [2-1:0]           keep_insert;
-  reg  [1-1:0]           byte_insert_cnt;
-  wire                   ready_insert;
+  reg                                valid_insert;
+  reg  [`DATA_WIDTH-1:0]             data_insert;
+  reg  [(`DATA_WIDTH / 8)-1:0]       keep_insert;
+  reg  [$clog2(`DATA_WIDTH / 8)-1:0] byte_insert_cnt;
+  wire                               ready_insert;
+  
+  integer                            data_wd_index;
 
   axi_stream_insert_header #(
-    .DATA_WD(16)
+    .DATA_WD(`DATA_WIDTH)
   ) axi_stream_insert_header_inst (
     .clk(clk),
     .rst_n(rst_n),
@@ -46,17 +50,17 @@ module skid_sim;
     .ready_insert(ready_insert)
   );
 
-          initial begin
+        initial begin
                 clk = 0;
                 forever #2 clk = ~clk;
-          end
-          
-          
-        // Reset and initialization
-          initial begin
+        end
+
+//         Reset and initialization
+        initial begin
+  
                 rst_n = 0;
                 valid_in = 0;
-                data_in = 255*32;
+                data_in = {$random($time)} % ((1 << `DATA_WIDTH)-1);
                 last_in = 0;
             
                 ready_out = 0;
@@ -67,60 +71,75 @@ module skid_sim;
                 byte_insert_cnt = 0;
             
                 #10 rst_n = 1;
-                #15 data_insert = 56;
-                byte_insert_cnt = 1;
+                #15 data_insert = {$random($time+1)} % ((1 << `DATA_WIDTH)-1);
+                byte_insert_cnt = {$random($time+2)} % ((1 << (`DATA_WIDTH / 8))-1);//$urandom_range(0, (`DATA_WIDTH / 8));
                 #20 @(posedge clk);
                 valid_insert = 1;
-          end
+        end
         
-          reg [9:0] cnt;
-        
+        reg [9:0] cnt;
+        integer breakdown_index; 
          // Input data generation
-          always @(posedge clk) begin
-            if (!rst_n) begin
-              cnt <= 0;
-              last_in <= 0;
-              keep_in <= 2'b00;
-            end
-            else begin
-              if (ready_in) begin
-                valid_in <= 1;
-                if (cnt % 150 == 0 && cnt) begin
-                  last_in <= 1;
-                  keep_in <= 2'b10;
+        always @(posedge clk) begin
+                if (!rst_n) begin
+                    cnt <= 0;
+                    last_in <= 0;
+                    keep_in <= 0;
                 end
-              end
-              else begin
-                valid_in <= 0;
-              end
-              if (ready_in && valid_in)
-                data_in <= data_in + 1;
-              else
-                data_in <= data_in;
-              cnt <= cnt + 1;
-            end
-          end
+                else begin
+                    if (ready_in && valid_in)
+                        data_in <= data_in + 1;
+                    else begin
+                        data_in <= data_in;
+                        end
+                    cnt <= cnt + 1;
+                    
+                                if (cnt % 75 == 0 && cnt) begin
+                                    last_in <= 1;
+                                    keep_in <= (1 << (`DATA_WIDTH / 8)) - (1 << ({$random($time+3)} % (`DATA_WIDTH / 8)));// 随机生成DATA_BYTE_WD位数，如32位的随机生成1111/1110/1100/1000
+/*                        极端情况：当last_in有效，即输入最后一个数据时，ready_out长期无效，是否能够有效接收到最后一个数据                          
+                                    for (breakdown_index = 16 ; breakdown_index > 0 ; breakdown_index = breakdown_index-1)begin
+                                    ready_out <= 0;    // 随机崩溃周期
+                                    @(posedge clk);
+                                    end
+                                    ready_out <= 1; 
+*/
+                               end
+                end
+        end
         
+         always @(posedge clk) begin
+            if (ready_in) begin
+                valid_in <= 1;
+            end
+            else begin
+                valid_in <= 0;
+            end
+         end
+             
           always @(posedge clk) begin
             if (!rst_n) begin
               cnt <= 0;
+              ready_out <= 1;
             end
             else begin
-              if (cnt % 9 == 0) begin
-                ready_out <= 0;
-                @(posedge clk);
-                @(posedge clk);
-                @(posedge clk);
-                ready_out <= 1;
-              end
+                
+                        for (breakdown_index = {$random}%16 ; breakdown_index > 0 ; breakdown_index = breakdown_index-1)begin
+                            ready_out <= 0;    // 随机崩溃周期
+                            @(posedge clk);
+                        end
+                        for (breakdown_index = {$random}%16 ; breakdown_index ; breakdown_index = breakdown_index-1)begin
+                            ready_out <= 1;    // 随机正常周期
+                            @(posedge clk);
+                        end
             end
-          end
+        end
         
         // Display output data
-          always @(posedge clk) begin
+        always @(posedge clk) begin
             if (ready_out && valid_out)
-              $display("Data is: %h", data_out);
-          end 
+            $display("Data is: %h, is the last one ? : %h, valid byte is %h", data_out, last_out, keep_out);
+        end 
           
           
 endmodule
